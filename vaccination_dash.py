a13148734e8fbf49e4abced9e273ed20dea675bd
James Kuht <james.kuht@gmail.com>
2021-07-21 09:11:28 +0100

    updated commit

--- vaccination_dash.py
+++ vaccination_dash.py
@@ -1,62 +1,34 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python vaccination_dash.py` and
+# Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
+
+df = pd.read_csv("owid-covid-data.csv")
+
+# generates a truncated table of all of the dataset
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text' : '#7FDBFF'
-}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello James',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(
-        children='Dash: A web application framework for Python.',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+app.layout = html.Div(children=[
+    html.H4(children='Covid vaccination data'),
+    generate_table(df)
 ])
 
-# by saying 'debug=True' means that the app reloads every time a change is made to the code
 if __name__ == '__main__':
     app.run_server(debug=True)
